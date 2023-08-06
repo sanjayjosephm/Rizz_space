bdb5664becc68a87f1619445220ba1a05d9bf8f6
sanjay <sanjayjosephm@gmail.com>
2023-08-06 19:22:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faMobileButton,faCode,faChartSimple,faPalette,faScrewdriverWrench} from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className='headline'>What We Offer</p>
+        <section className='FlexContainer'>
+            <div className='flex-items one'>
+              <div className='vetor-image'>
+                <FontAwesomeIcon icon={faMobileButton} className='icons'/>
+              </div>
+              <p className='title'>App Development</p>
+              <span></span>
+              <p className='content'>From concept to launch, our skilled app development team crafts seamless and innovative mobile and web applications ....</p>
+              <p className="read-more">Read More</p>
+            </div>
+            <div className='flex-items two'>
+                <div className='vetor-image'>
+                  <FontAwesomeIcon icon={faCode} className='icons'/>
+                </div>
+                <p className='title'>Web Development</p>
+                <span></span>
+                <p className='content'>We create responsive, user-friendly websites that combine stunning design with robust functionality.</p>
+                <p className="read-more">Read More</p>
+              </div>
+            <div className='flex-items three'>
+                <div className='vetor-image'>
+                <FontAwesomeIcon icon={faPalette} className='icons'/>
+                </div>
+                <p className='title'>UI/UX Designing</p>
+                <span></span>
+                <p className='content'>Our UI/UX designers turn user journeys into visually stunning and intuitive interfaces, elevating user engagement and .....</p>
+                <p className="read-more">Read More</p>
+              </div>
+            <div className='flex-items four'>
+              <div className='vetor-image'>
+              <FontAwesomeIcon icon={faScrewdriverWrench} className='icons'/>
+              </div>
+              <p className='title'>Rebranding</p>
+              <span></span>
+              <p className='content'>Our UI/UX designers turn user journeys into visually stunning and intuitive interfaces, elevating user engagement and .....</p>
+              <p className="read-more">Read More</p>
+            </div>
+            <div className='flex-items five'>
+              <div className='vetor-image'>
+              <FontAwesomeIcon icon={faChartSimple} className='icons'/>
+              </div>
+              <p className='title'>Market Research</p>
+              <span></span>
+              <p className='content'>Our UI/UX designers turn user journeys into visually stunning and intuitive interfaces, elevating user engagement and .....</p>
+              <p className="read-more">Read More</p>             
+            </div>
+        </section>
     </div>
   );
 }
